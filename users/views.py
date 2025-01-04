327dcca28bdc30f406dd014bde0aa0345ec9f7ee
Your Name <toomelaaimar@gmail.com>
2025-01-04 22:46:53 +0200

    user login and register now available

---
+++ users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import RegisterForm, LoginForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+@login_required
+def home_view(request):
+    return render(request, 'users/home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
